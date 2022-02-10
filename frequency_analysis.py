254658d11950a1ebe8126bd5a1aa65505b2458b9
xelex <xelex2007@gmail.com>
2022-02-10 14:42:05 +0300

    debug

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -8,7 +8,7 @@ def get_key(d, value):
 alphabet={'а':0,'б':0,'в':0,'г':0,'д':0,'е':0,'ё':0,'ж':0,'з':0,'и':0,'й':0,'к':0,'л':0,'м':0,'н':0,'о':0,'п':0,'р':0,'с':0,'т':0,'у':0,'ф':0,'х':0,'ц':0,'ч':0,'ш':0,'щ':0,'ъ':0,'ы':0,'ь':0,'э':0,'ю':0,'я':0}
 symbols_frequency={'а':8.01,'б':1.59,'в':4.54,'г':1.70,'д':2.98,'е':8.54,'ё':0.04,'ж':0.94,'з':1.65,'и':7.35,'й':1.21,'к':3.49,'л':4.40,'м':3.21,'н':6.70,'о':10.97,'п':2.81,'р':4.73,'с':5.47,'т':6.26,'у':2.62,'ф':0.26,'х':0.97,'ц':0.48,'ч':1.44,'ш':0.73,'щ':0.36,'ъ':0.04,'ы':1.90,'ь':1.74,'э':0.32,'ю':0.64,'я':2.01}
 symbol_sum=0
-special_symbols=('.',',','-',' ','1','2','3','4','5','6','7','8','9','0','\n','?','!')
+special_symbols=('.',',','-',' ','1','2','3','4','5','6','7','8','9','0','\n','?','!','–',':')
 results={}
 file_name = input('Input file path for truing to decrypt: ')
 fp = open(file_name, 'r', encoding='utf-8')
@@ -50,8 +50,8 @@ for string_item in fp:
                 difference[s_item]=round(abs(results[character_item.lower()]-symbols_frequency[s_item]),2)
             # print('Symbol for replacement: ')
             # print(get_key(difference,min(difference.values())))
-            print(difference)
-            # fd.write(get_key(difference,min(difference.values())))
+            # print(difference)
+            fd.write(get_key(difference,min(difference.values())))
             # # print(results[symbol_item])
             # # print(symbols_frequency[symbol_item])
             # if (math.isclose(results[symbol_item],symbols_frequency[symbol_item])):
